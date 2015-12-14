cd7ae28235c951956a03a05596b710973945cae7
jql <jql@jql-pc.(none)>
2015-12-14 09:35:29 -0500

    run a thread

---
+++ fdfs_global.h
@@ -0,0 +1,14 @@
+#ifndef __FDFS_GLOBALH
+#define __FDFS_GLOBALH
+
+#include "fdfs_define.h"
+
+ int g_server_port = FDFS_STORAGE_SERVER_DEF_PORT;
+ int g_max_connections = FDFS_DEF_MAX_CONNECTONS;
+
+ bool g_continue_flag = true;
+
+ pthread_mutex_t g_tracker_thread_lock;
+ int g_tracker_thread_count = 0;
+
+#endif
---
+++ tracker_service.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <pthread.h>
+#include "tracker_service.h"
+
+void* tracker_thread_entrance(void* arg)
+{
+	printf("tracker_thread_entrance done!\n");
+
+	while(1)
+	{
+		printf("thread id %x\n", pthread_self());
+		sleep(2);
+	}
+}
